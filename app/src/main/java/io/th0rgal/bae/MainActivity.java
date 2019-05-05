972cf0610e853aee425ff2a27533347302b833d5
Thomas Marchand <thomas.marchand44@gmail.com>
2019-05-05 20:50:57 +0200

    design started

--- app/src/main/java/io/th0rgal/bae/MainActivity.java
+++ app/src/main/java/io/th0rgal/bae/MainActivity.java
@@ -1,5 +1,7 @@
 package io.th0rgal.bae;
 
+import android.view.Menu;
+import android.view.MenuInflater;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,6 +10,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle(R.string.app__full_name);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
